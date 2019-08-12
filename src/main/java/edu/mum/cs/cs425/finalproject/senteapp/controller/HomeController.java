6663c519ba5dff85b86c42c593a7c3cd6f2190ab
mniyonshuti <niyomoses1992@gmail.com>
2019-08-12 15:29:33 -0500

    POJOs

---
+++ src/main/java/edu/mum/cs/cs425/finalproject/senteapp/controller/HomeController.java
@@ -0,0 +1,7 @@
+package edu.mum.cs.cs425.finalproject.senteapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+}
---
+++ src/main/java/edu/mum/cs/cs425/finalproject/senteapp/model/Member.java
@@ -0,0 +1,114 @@
+package edu.mum.cs.cs425.finalproject.senteapp.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "members")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+    private Long memberNumber;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "addressId")
+    private Address address;
+    private String contactNumber;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "recordId")
+    private  Record record;
+
+
+    public Member() {
+    }
+
+    public Member(Long memberId, Long memberNumber, String firstName, String middleName, String lastName, Address address, String contactNumber, Record record) {
+        this.memberId = memberId;
+        this.memberNumber = memberNumber;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.address = address;
+        this.contactNumber = contactNumber;
+        this.record = record;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public Long getMemberNumber() {
+        return memberNumber;
+    }
+
+    public void setMemberNumber(Long memberNumber) {
+        this.memberNumber = memberNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public Record getRecord() {
+        return record;
+    }
+
+    public void setRecord(Record record) {
+        this.record = record;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "memberId=" + memberId +
+                ", memberNumber=" + memberNumber +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address=" + address +
+                ", contactNumber='" + contactNumber +
+                '}';
+    }
+}
