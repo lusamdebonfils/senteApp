5003711b67fa622b1fb66c8b7d7d33357caddb46
mniyonshuti <52430728+mniyonshuti@users.noreply.github.com>
2019-08-11 19:03:29 -0500

    Add files via upload

---
+++ senteapp/src/main/java/edu/mum/cs/cs425/finalproject/senteapp/model/Manager.java
@@ -0,0 +1,9 @@
+package edu.mum.cs.cs425.finalproject.senteapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "managers")
+public class Manager {
+}
---
+++ senteapp/src/main/java/edu/mum/cs/cs425/finalproject/senteapp/model/Record.java
@@ -0,0 +1,71 @@
+package edu.mum.cs.cs425.finalproject.senteapp.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "records")
+public class Record {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long recordId;
+    private Long recordNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "loan_id")
+    private List<Loan> loanList;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "saving_id")
+    private List<Saving> savingList;
+
+    public Record() {
+    }
+
+    public Record(Long recordNumber) {
+        this.recordNumber = recordNumber;
+        this.loanList = new ArrayList<>();
+        this.savingList = new ArrayList<>();
+    }
+
+    public Long getRecordId() {
+        return recordId;
+    }
+
+    public void setRecordId(Long recordId) {
+        this.recordId = recordId;
+    }
+
+    public Long getRecordNumber() {
+        return recordNumber;
+    }
+
+    public void setRecordNumber(Long recordNumber) {
+        this.recordNumber = recordNumber;
+    }
+
+    public List<Loan> getLoanList() {
+        return loanList;
+    }
+
+    public void setLoanList(List<Loan> loanList) {
+        this.loanList = loanList;
+    }
+
+    public List<Saving> getSavingList() {
+        return savingList;
+    }
+
+    public void setSavingList(List<Saving> savingList) {
+        this.savingList = savingList;
+    }
+
+    @Override
+    public String toString() {
+        return "Record{" +
+                "recordId=" + recordId +
+                ", recordNumber=" + recordNumber +
+                ", loanList=" + loanList +
+                ", savingList=" + savingList +
+                '}';
+    }
+}
